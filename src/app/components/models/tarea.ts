01717cde3798a99dd03e77a2b8955c60d6dd6286
Roberto <mario24roberto@gmail.com>
2021-11-02 12:39:27 -0600

    Agregando el evento keyup.enter

--- src/app/components/models/tarea.ts
+++ src/app/components/models/tarea.ts
@@ -2,7 +2,7 @@ export class tarea{
     nombre:string;
     estado:boolean
 
-    constructor(nombre:string, estado:string){
+    constructor(nombre:string, estado:boolean){
         this.nombre = nombre;
         this.estado = estado;
     }
--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tarea } from '../models/tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent implements OnInit {
-  PlaceHolder='Ingrese Tarea'
+  PlaceHolder='Ingrese Tarea';
+  listTareas :tarea[] =[];
+  nombreTarea = '';
   constructor() { }
 
   ngOnInit(): void {
   }
+agregarTarea(){
+  console.log(this.nombreTarea);
+  // Objeto Tarea
 
+  // Agregar El objeto tarea al array
+
+  // Reset Form
+}
 }
